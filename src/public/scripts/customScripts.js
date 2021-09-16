acd18131720b1900040205a5268f819866a5955d
cvegabello <carlosvegabello@gmail.com>
2021-09-16 01:43:16 -0400

    change of links card

--- src/public/scripts/customScripts.js
+++ src/public/scripts/customScripts.js
@@ -37,6 +37,22 @@ _changePhotoBtn.addEventListener("click", function () {
 });
 
 
+// var getFavicon = function(){
+//   var favicon = undefined;
+//   var nodeList = document.getElementsByTagName("link");
+//   for (var i = 0; i < nodeList.length; i++)
+//   {
+//       if((nodeList[i].getAttribute("rel") == "icon")||(nodeList[i].getAttribute("rel") == "shortcut icon"))
+//       {
+//           favicon = nodeList[i].getAttribute("href");
+//       }
+//   }
+//   return favicon;        
+// }
+
+// alert(getFavicon());​
+
+
 
 
 
